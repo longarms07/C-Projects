807281a1c73616ae8806152dd29c3b578681285c
longarms07 <ecridd@gmail.com>
2020-02-06 15:40:22 -0500

    Added recursive version of prime test.

--- Week2HW/prime_test_recursive.cpp
+++ Week2HW/prime_test_recursive.cpp
@@ -22,8 +22,11 @@ int main()
 }
 
 bool CheckPrime(int number){
+
+    bool CheckPrimeDivisors(float, float);
+
     //0, 1, and 2 are prime so no need to check. 
-    //2 is included since the next check determines if it is divisible by 2 (even).
+    //2 is included since the next check determines if it is divisible by 2 (or other even numbers as a result).
     if(number <= 2) return true;
     float numberF = static_cast<float>(number);
     //check if the number divides by 2. If the number is a whole number it is not prime.
@@ -35,14 +38,20 @@ bool CheckPrime(int number){
     }
     //Next, we will divide by each odd number between 2 and the square root of the number being tested.
     float root = floor(static_cast<float>(sqrt(number)));
-    for(float i = 3.0; i<=root; i+=2.0){
-        float result = numberF/i;
-        if(result == floor(result)){
-            cout << number << " is divisible by " << i << "!\n";
+    //If the root is even, subtract by 1 to get the first odd number to check. 
+    if((static_cast<int>(root) % 2) ==0) root--;
+    return CheckPrimeDivisors(number, root);
+}
+
+bool CheckPrimeDivisors(float number, float divisor){
+    //cout << "Divisor is "<<divisor<<"\n";
+    if(divisor <= 2) return true;
+    float result = number/divisor;
+    if(result == floor(result)){
+            cout << number << " is divisible by " << divisor << "!\n";
             return false;
-        }
     }
-    return true;
+    return (CheckPrimeDivisors(number, divisor-2.0));
 }
 
 
