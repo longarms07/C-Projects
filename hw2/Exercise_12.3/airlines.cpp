f8bf87960e0596f41015bc45086cbb5bb121a288
longarms07 <ecridd@gmail.com>
2020-02-15 12:19:13 -0500

    completed 12.3

--- hw2/Exercise_12.3/airlines.cpp
+++ hw2/Exercise_12.3/airlines.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <list>
+#include <algorithm> //allows use of std::transform
 
 using namespace std;
 
@@ -22,11 +23,13 @@ struct flight{
 };
 
 struct airport{
+    string name;
     string code; //length 3, dictionary key
     list<flight> flights; //List of all flights departing from this airport
 };
 
 airport sarasota = {
+    "Sarasota International",
     "SIA",
     {
         {
@@ -36,10 +39,25 @@ airport sarasota = {
             {8,55},
             {18,27}
         },
+        {
+            5801,
+            "SIA",
+            "GRA",
+            {12,17},
+            {23,59}
+        },
+        {
+            5802,
+            "SIA",
+            "ENT",
+            {14,30},
+            {15,32}
+        },
     }
 };
 
 airport aaaland = {
+    "Aland Aviation",
     "AAA",
     {
         {
@@ -56,8 +74,128 @@ airport aaaland = {
             {5,25},
             {12,15}
         },
+        {
+            4802,
+            "AAA",
+            "SIA",
+            {9,25},
+            {16,15}
+        },
+        {
+            4803,
+            "AAA",
+            "GMM",
+            {12,38},
+            {22,18}
+        },
+        {
+            4804,
+            "AAA",
+            "SIA",
+            {13,25},
+            {20,15}
+        },
     }
 };
+
+airport galar = {
+    "Galar Regional",
+    "GRA",
+    {
+        {
+            1299,
+            "GRA",
+            "SIA",
+            {0,38},
+            {13,10}
+        },
+        {
+            1300,
+            "GRA",
+            "GMM",
+            {8,55},
+            {10,38}
+        },
+        {
+            1301,
+            "GRA",
+            "ENT",
+            {12,45},
+            {15,47}
+        },
+    }
+};
+
+airport enterprise = {
+    "Enterprise NX-01",
+    "ENT",
+    {
+        {
+            1008,
+            "ENT",
+            "SIA",
+            {2,30},
+            {3,30}
+        },
+        {
+            1009,
+            "ENT",
+            "GRA",
+            {8,15},
+            {11,18}
+        },
+        {
+            1009,
+            "ENT",
+            "GMM",
+            {13,58},
+            {20,27}
+        },
+        {
+            1010,
+            "ENT",
+            "SIA",
+            {15,30},
+            {23,30}
+        },
+        {
+            1011,
+            "ENT",
+            "GRA",
+            {18,15},
+            {18,37}
+        },
+    }
+};
+
+airport fodlan = {
+    "Garreg Mach Monastery",
+    "GMM",
+    {
+        {
+            9996,
+            "GMM",
+            "GRA",
+            {8,17},
+            {10,30}
+        },
+        {
+            9996,
+            "GMM",
+            "ENT",
+            {12,38},
+            {15,24}
+        },
+        {
+            9996,
+            "GMM",
+            "SIA",
+            {18,10},
+            {23,11}
+        },
+    }
+};
+
 
 map<string, airport> airports;
 
@@ -66,7 +204,48 @@ void flight_list_to_print(list<flight> flights);
 int main(){
     airports[sarasota.code] = sarasota;
     airports[aaaland.code] = aaaland;
-    flight_list_to_print(airports["AAA"].flights);
+    airports[galar.code] = galar;
+    airports[enterprise.code] = enterprise;
+    airports[fodlan.code] = fodlan;
+    //flight_list_to_print(airports["AAA"].flights);
+    
+    cout << "The following airports are avaliable: \n";
+    for(map<string, airport>::iterator airports_iterator = airports.begin(); 
+        airports_iterator != airports.end(); ++airports_iterator){
+            cout << airports_iterator->second.name << " Airport : " << airports_iterator->first <<"\n"; 
+    }
+    string input_code = "";
+    string code_0 = "";
+    string code_1 = "";
+    string adjective = "first";
+    while(code_0 == "" || code_1 == ""){
+        cout << "Enter the " << adjective << " airport to get a list of all flights leaving from it today. \n";
+        cin >> input_code;
+        transform(input_code.begin(), input_code.end(), input_code.begin(), ::toupper); //performs toupper on each char in the string
+        if(input_code.length() != codeLength){
+            cout << "Error! The code you entered is the wrong length. All airport codes must be exactly 3 characters.\n";
+            //return(2);
+        }
+        else if(airports.find(input_code) == airports.end()){
+            cout << "Error! You entered an invalid airport code. Please refer back to the airport list.\n";
+            //return(3);
+        }
+        else if(code_0 == input_code){
+            cout << "You already chose this airport, please chose another.\n";
+        }
+        else{
+            if(code_0 == ""){
+                code_0 = input_code;
+                adjective = "second";
+            }
+            else
+            {
+                code_1 = input_code;
+            }
+        }
+    }
+    flight_list_to_print(airports[code_0].flights);
+    flight_list_to_print(airports[code_1].flights);
 }
 
 void flight_list_to_print(list<flight> flights){
